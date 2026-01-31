a0cf6623216be2403ecac015dd754884d256c77c
Siddharth-Andhale <sidd1212004@gmail.com>
2026-01-31 13:01:30 +0530

    Fix: Restore missing headers (spi.h, gpio.h) for 6.12 build

--- kernel/panel-ili9486.c
+++ kernel/panel-ili9486.c
@@ -11,6 +11,14 @@
 
 #include <linux/delay.h>
 #include <linux/module.h>
+#include <linux/spi/spi.h>
+#include <linux/gpio/consumer.h>
+
+#include <drm/drm_mipi_dbi.h>
+#include <drm/drm_print.h>
+#include <drm/drm_drv.h>
+#include <drm/drm_gem_dma_helper.h>
+#include <drm/drm_probe_helper.h>
 #include <drm/drm_atomic_helper.h>
 
 /* ... (previous includes) ... */
